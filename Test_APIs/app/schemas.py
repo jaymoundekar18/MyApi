a76eba712cdbdd8f4a712cc7ffc589cade003c85
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2026-02-12 10:31:21 +0530

    Update schemas.py

--- Test_APIs/app/schemas.py
+++ Test_APIs/app/schemas.py
@@ -1,5 +1,10 @@
+from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from typing import Optional
+
+# -------------------------------------------------
+# Employee schemas for request and response models
+# -------------------------------------------------
 
 class EmployeeBase(BaseModel):
     firstname: str
@@ -14,3 +19,36 @@ class EmployeeCreate(EmployeeBase):
 
 class EmployeeResponse(EmployeeBase):
     id: str
+
+
+# -------------------------------------------------
+# E-commerce schemas for request and response models
+# -------------------------------------------------
+
+class UserResponse(BaseModel):
+    id: str
+    name: str
+    email: EmailStr
+    country: str
+    age: int
+    signup_date: datetime
+
+class ProductResponse(BaseModel):
+    id: str
+    name: str
+    price: float
+    category: str
+    in_stock: int
+
+class OrderItem(BaseModel):
+    product_id: str
+    quantity: int
+    price: float
+
+class OrderResponse(BaseModel):
+    id: str
+    user_id: str
+    items: list[OrderItem]
+    total_amount: float
+    order_date: datetime
+    status: str
