e2a60b56d7a44442e4293ed7513223708ec3d773
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2026-02-12 10:34:20 +0530

    Update curd.py

--- Test_APIs/app/curd.py
+++ Test_APIs/app/curd.py
@@ -1,6 +1,11 @@
 from bson import ObjectId
 from app.database import employee_collection
-from app.models import employee_helper
+from app.database import users_collection, products_collection, orders_collection
+from app.models import employee_helper, user_helper, product_helper, order_helper
+
+# ---------------------------
+# Employee CURD operations
+# ---------------------------
 
 # CREATE
 def add_employee(employee: dict):
@@ -36,3 +41,20 @@ def update_employee(id: str, data: dict):
 def delete_employee(id: str):
     employee_collection.delete_one({"_id": ObjectId(id)})
     return True
+
+
+# ---------------------------
+# E-commerce operations
+# ---------------------------
+
+# Get all users
+def get_all_users():
+    return [user_helper(user) for user in users_collection.find()]
+
+# Get all products
+def get_all_products():
+    return [product_helper(product) for product in products_collection.find()]
+
+# Get all orders
+def get_all_orders():
+    return [order_helper(order) for order in orders_collection.find()]
